be989436eeaf72ca5deb3835b039c6a13ef7e18b
zzyzzy <zzyzigy@gmail.com>
2025-01-16 11:57:45 +0900

    resources 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+-- add image data
+INSERT INTO images (description, url) VALUES('vulture on tree', 'images/vulture.png');
+INSERT INTO images (description, url) VALUES('female lions', 'images/3-female-lions.png');
+INSERT INTO images (description, url) VALUES('antelopes', 'images/antelopes.png');
+INSERT INTO images (description, url) VALUES('birds', 'images/birds.png');
+INSERT INTO images (description, url) VALUES('buffalo', 'images/buffalo.png');
+INSERT INTO images (description, url) VALUES('cheetah', 'images/cheetah.png');
+INSERT INTO images (description, url) VALUES('elephants', 'images/elephants.png');
+INSERT INTO images (description, url) VALUES('jackal', 'images/jackal.png');
+INSERT INTO images (description, url) VALUES('giraffe', 'images/giraffe.png');
+INSERT INTO images (description, url) VALUES('hippos', 'images/hippos.png');
+INSERT INTO images (description, url) VALUES('male lion', 'images/male-lion.png');
+INSERT INTO images (description, url) VALUES('zebra', 'images/zebra.png');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE images
+(
+    imageid INT AUTO_INCREMENT PRIMARY KEY,
+    description VARCHAR(512) NOT NULL,
+    url VARCHAR(128) NOT NULL
+);
